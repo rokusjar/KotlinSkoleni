e485b4b6a9b043a35f02bab26fa77fec0c0fd7fd
Jaromír Rokusek <jrokusek@csob.cz>
2018-10-15 10:17:00 +0200

    - todo modification

--- src/main/kotlin/nullsafety/nullsafety.kt
+++ src/main/kotlin/nullsafety/nullsafety.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
 
     // TODO (1) Vytvořte objekt třídy Person s názvem person, kde property adress má hodnotu null. (Nápověda: nebojte se upravit třídu Person)
 
-    // TODO (2) Napište kód který vezme objekt person a vypíše do konzole město a stát a zároveň ošetřuje případ kdy je adress null
+    // TODO (2) Napište kód který vezme objekt person a vypíše do konzole město a stát a zároveň ošetřuje případ kdy je adresa null
 
-    // TODO (3) Vylepšte výpis do konzole tím, že přidáte nějakou výchozí hodnotu, která se vypíše pokud adresa není známá
+    // TODO (3) Vylepšete výpis do konzole tím, že přidáte nějakou výchozí hodnotu, která se vypíše pokud je adresa null
 }
