9c7187073c287926d10d257a26aa10935c293080
Jaromír Rokusek <jrokusek@csob.cz>
2018-10-15 09:28:31 +0200

    - collection example preparation

---
+++ src/main/kotlin/collections/collections.kt
@@ -0,0 +1,13 @@
+package collections
+
+fun main(args: Array<String>) {
+
+    var mutableColors = mutableListOf("red", "green", "blue")
+
+    // TODO (1) Vytvořte read-only(immutable) pohled na kolekci mutableColors
+
+    // TODO (2) Do kolekce mutbale Colors přidejte další barvu
+
+    // TODO (3) Vypište do konzole obsah pohledu, ale odfiltrujte červenou
+
+}
