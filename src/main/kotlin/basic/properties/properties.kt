a3ccc1210a0061816d21262f7623df8d0474a1b2
Jaromír Rokusek <jrokusek@csob.cz>
2018-10-16 14:45:05 +0200

    - properties example solution

--- src/main/kotlin/basic/properties/properties.kt
+++ src/main/kotlin/basic/properties/properties.kt
@@ -1,25 +1,44 @@
 package basic.properties
 
+import kotlin.properties.Delegates
+
 
 class Person(val firstName: String, val lastName: String) {
 
-    // TODO (1) Přidejte property fullName s custom getterm, která bude spojovat firstName a lastName
+    val fullName: String
+        get() = "${this.firstName} ${this.lastName}"
 
-    /*
-        TODO (2) Přidejte property nickname typu var s výchozí hodnotou Unknown, custom getterem který vrátí "firstName AKA nickname"
-        TODO a custom setterm který nejdřív zkontroluje nastavovanou hodnotu a nastaví ji pouze pokud není blank
-     */
+    var nickname: String = "Unknown"
+        get() = "$firstName AKA $field"
+        set(value) {
+            if (value.isNotBlank()) field = value
+        }
 
 
     // TODO (3) Přidejte lateinit property adress typu Adress a metodu setupAdress která ji inicializuje
+    lateinit var adress: Adress
 
-    // TODO (4) Přidejte property age a delegujte ji na existujícího delegáta observable. Pomocí něj vypište do konzole jak se mění hodnota age
+    fun setup() {
+        adress = Adress("London", "Baker street")
+    }
+
+    var age: Int by Delegates.observable(0,
+            onChange = { property, oldValue, newValue -> println("Age changed from $oldValue to $newValue") })
 }
 
 class Adress(val city: String, val street: String)
 
 
 fun main(args: Array<String>) {
+    val person = Person("John", "Wick")
+    person.nickname = "Bogeyman"
 
-    // TODO vyzkoušejte třídu Person
+    println("Full name: ${person.fullName}")
+    println("Nickname: ${person.nickname}")
+
+    person.setup()
+    println("Full name with nickname: ${person.adress}")
+
+    person.age = 32
+    println("Age: ${person.age}")
 }
