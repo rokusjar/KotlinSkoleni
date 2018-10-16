1fc0ae5c59fde8f0a703df88820b144461f68221
Jaromír Rokusek <jrokusek@csob.cz>
2018-10-16 10:04:55 +0200

    - collections solution

--- src/main/kotlin/basic/collections/collections.kt
+++ src/main/kotlin/basic/collections/collections.kt
@@ -5,9 +5,11 @@ fun main(args: Array<String>) {
     var mutableColors = mutableListOf("red", "green", "blue")
 
     // TODO (1) Vytvořte read-only(immutable) pohled na kolekci mutableColors
+    val immutableView: List<String> = mutableColors
 
     // TODO (2) Do kolekce mutable Colors přidejte další barvu
+    mutableColors.add("purple")
 
     // TODO (3) Vypište do konzole obsah pohledu, ale odfiltrujte červenou
-
+    println(immutableView.filter { it != "red" })
 }
