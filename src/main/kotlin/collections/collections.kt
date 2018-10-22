f5842ef5811face2fd6159a8adf58c08acc3674c
Jaromír Rokusek <jrokusek@csob.cz>
2018-10-22 14:15:45 +0200

    - typo fix

--- src/main/kotlin/collections/collections.kt
+++ src/main/kotlin/collections/collections.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
 
     // TODO (1) Vytvořte read-only(immutable) pohled na kolekci mutableColors
 
-    // TODO (2) Do kolekce mutbale Colors přidejte další barvu
+    // TODO (2) Do kolekce mutable Colors přidejte další barvu
 
     // TODO (3) Vypište do konzole obsah pohledu, ale odfiltrujte červenou
 
