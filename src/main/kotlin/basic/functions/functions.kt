9e59d399a344a3e0a962cb30b1acee87ed1d11a3
Jaromír Rokusek <jrokusek@csob.cz>
2018-11-05 12:48:51 +0100

    - todo returned

--- src/main/kotlin/basic/functions/functions.kt
+++ src/main/kotlin/basic/functions/functions.kt
@@ -2,12 +2,16 @@ package basic.functions
 
 fun main(args: Array<String>) {
 
+    // TODO (1) Doplntě nějakou výchozí hodnotu pro příjmení
     fun printMyName(firstName: String, lastName: String = "Doe") = println("Jmenuji se $firstName $lastName")
 
+    // TODO (2) Zkuste zavolat funkci printMyName s pojmenováním parametrů, zkuste prohodit hodnoty
     printMyName(lastName = "Lucas", firstName = "George")
 
+    // TODO (3) Předělejte funkci tak, aby příjímala jinou funkci jako druhý parametr, zavolala ji na parametr string a vrátila výsledek této operace
     fun callOnString(string: String, function: String.() -> String): String = string.function()
 
+    // TODO (4) Zkuste zavolat upravenou funkci callOnString,  funkci si vymyslete, nebo použijte nějakou existující
     println(callOnString("Harrison Ford", String::reversed))
 
     println(callOnString("Harrison Ford") {
@@ -16,9 +20,11 @@ fun main(args: Array<String>) {
 
     })
 
+    // TODO (5) Vytvořte rozšiřující funkci pro třídu String, která vypíše string na který je zavolána (receiver) do konzole.
     fun String.println() = println(this)
     "Star Wars".println()
 
+    // TODO (6) Vytvořte "infix function" splitBy která se bude volat na String a bude přijímat Char, podle kterého bude String dělit
     infix fun String.splitBy(char: Char): List<String> = this.split(char)
     println("May the force be with you!" splitBy ' ')
 }
