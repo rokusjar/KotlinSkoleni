a816116f8c185a6bb0c45bc4f1d635d04c6d9970
Jaromír Rokusek <jrokusek@csob.cz>
2018-11-05 12:55:09 +0100

    - todo returned

--- src/main/kotlin/basic/generics/generics.kt
+++ src/main/kotlin/basic/generics/generics.kt
@@ -4,6 +4,7 @@ interface Eatable
 open class Fruit
 class Apple : Fruit(), Eatable
 
+// TODO (1) Doplňte omezení pro generický typ T, měl by dědit třídu Fruit a implementovat rozhraní Eatable
 class FruitBasket<T> where T : Fruit, T : Eatable
 
 interface FruitComparator<in T> {
@@ -19,9 +20,11 @@ fun main(args: Array<String>) {
     val fruitComparator: FruitComparator<Fruit> = object : FruitComparator<Fruit> {
         override fun compare(first: Fruit, second: Fruit) = TODO("Pro účely tohoto cvičení není třeba implementovat")
     }
+    // TODO (2) Uložte do proměnné appleComparator připravený fruitComparator. Upravte kód tak, aby nehlásil chybu.
     val appleComparator: FruitComparator<Apple> = fruitComparator
 
 
     val appleProducer: FruitProducer<Apple> = FruitProducer()
+    // TODO (3) Uložte do proměnné fruitProducer připravený appleProducer. Upravte kód tak, aby nehlásil chybu.
     val fruitProducer: FruitProducer<Fruit> = appleProducer
 }
