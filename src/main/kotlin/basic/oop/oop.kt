aa54fc0c002eaeca6a2fc906a4a1b5e71c8824e6
Jaromír Rokusek <jrokusek@csob.cz>
2018-11-05 13:53:38 +0100

    - solution reworked - todo returned

--- src/main/kotlin/basic/oop/oop.kt
+++ src/main/kotlin/basic/oop/oop.kt
@@ -1,7 +1,9 @@
 package basic.oop
 
-abstract class Shape(val type: Type) {
+interface Shape {
     // TODO (1.1) Doplntě property která bude popisovat o jaký tvar se jedná (Rectangle, Circle)
+
+    val type: Type
 
     abstract fun area(): Double
 
@@ -9,11 +11,14 @@ abstract class Shape(val type: Type) {
 
     enum class Type {
         RECTANGLE,
-        CIRCLE
+        CIRCLE,
+        SQUARE
     }
 }
 
-class Circle(private val radius: Double) : Shape(Type.CIRCLE) {
+class Circle(private val radius: Double) : Shape {
+
+    override val type: Shape.Type = Shape.Type.CIRCLE
 
     // TODO (1.2) použijte třídu Shape jako abstratkního předka
 
@@ -23,7 +28,9 @@ class Circle(private val radius: Double) : Shape(Type.CIRCLE) {
     // TODO (1.4) přesuňte do abstraktní třídy Shape a slovo Circle nahraďte property
 }
 
-class Rectangle(private val length: Double, private val width: Double) : Shape(Type.RECTANGLE) {
+class Rectangle(private val length: Double, private val width: Double) : Shape {
+
+    override val type: Shape.Type = Shape.Type.RECTANGLE
 
     // TODO (1.5) použijte třídu Shape jako abstratkního předka
 
@@ -33,8 +40,16 @@ class Rectangle(private val length: Double, private val width: Double) : Shape(T
     // TODO (1.7) přesuňte do abstraktní třídy Shape a slovo Rectangle nahraďte property
 
     // TODO (6.1) přidejte sekundární konstruktor, který bude přijímat pouze jeden rozměr, objekt bude představovat čtverec.
+    constructor(size: Double) : this(length = size, width = size)
 
-    // TODO (6.2) zajistěte, že se při vytvoření objektu Rectangle vypíše do konzole, zda se jedná o čtverec, nebo ne
+    // TODO (6.2) zajistěte, že se při vytvoření objektu Rectangle vypíše do konzole, zda se jedná o čtverec, nebo obdélník
+    init {
+        if (length.compareTo(width) == 0) {
+            println("Square of size $length has been created")
+        } else {
+            println("Rectangle of length: $length and width: $width has been created")
+        }
+    }
 }
 
 fun main(args: Array<String>) {
@@ -42,7 +57,7 @@ fun main(args: Array<String>) {
     // TODO (1) přepracujte třídy Rectangle, Circle a Shape
 
     // TODO (2) inicializujte kolekci, naplňte ji různými tvary (Rectangle, Circle)
-    val shapes: List<Shape> = listOf(
+    val shapes: MutableList<Shape> = mutableListOf(
             Circle(4.0),
             Rectangle(2.0, 4.0),
             Circle(1.5)
@@ -56,6 +71,16 @@ fun main(args: Array<String>) {
     // TODO (4) Abstraktní třídu nahraďte rozhraním
 
     // TODO (5) Doplňte do kolekce shapes čtverec, který definujete pomocí anonymní třídy
+    shapes.add(object : Shape {
+        override val type: Shape.Type = Shape.Type.SQUARE
+        override fun area(): Double = 25.0
+    })
+
+    shapes.add(Rectangle(length = 3.0, width = 3.0))
+
+    for(shape in shapes) {
+        println("Shape of type: ${shape.type} with area equal to: ${shape.area()}")
+    }
 
     // TODO (6) Doplntě další konstruktor pro třídu Rectangle
 }
