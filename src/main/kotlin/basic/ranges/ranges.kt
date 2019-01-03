8dc3ad628409d9a71113ea391f5b348c2ca00dd5
Jaromír Rokusek <jrokusek@profinit.eu>
2019-01-03 15:43:40 +0100

    - range example added

---
+++ src/main/kotlin/basic/ranges/ranges.kt
@@ -0,0 +1,12 @@
+package basic.ranges
+
+fun main(args: Array<String>) {
+
+    val x = 3
+
+    // TODO (1) Upravte následující kód. Použijte ranges a operátor in.
+    if (x >= 0 && x <= 10) {
+        println("$x is in range")
+    }
+
+}
