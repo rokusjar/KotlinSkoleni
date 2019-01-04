f9f807eba66d83c8a0e2d94496754f11fe59d8e0
Jaromír Rokusek <jrokusek@profinit.eu>
2019-01-04 09:31:26 +0100

    - casting and conversion solution added

--- src/main/kotlin/basic/castingandconversion/castingandconversion.kt
+++ src/main/kotlin/basic/castingandconversion/castingandconversion.kt
@@ -5,13 +5,18 @@ fun main(args: Array<String>) {
     val a: Any = "aaa"
 
     // TODO (1) Odkomentujte a obalte volání funkce println podmínkou, která vyřeší "Unresolved reference"
-    //println(a.isEmpty())
+    if (a is String) {
+        println(a.isEmpty())
+    }
 
     /*
     TODO (2) Ověřte jestli a je typu String. Pokud ano rovnou uložte hodnotu a do proměnné b,
     TODO     pokud ne, převeďte a na String a pak uložtej její hodnotu do b.
     */
     // Nápověda: Použijte when
-    val b: String
+    val b: String = when (a) {
+        is String -> a
+        else -> a.toString()
+    }
 
 }
