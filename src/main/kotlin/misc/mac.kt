3039c3dc47d7b91ea0335f5c08cc50560d3d255b
Marcel Matula <marcel.matula@gmail.com>
2019-01-26 19:18:27 +0100

    mac convert todo task

---
+++ src/main/kotlin/misc/mac.kt
@@ -0,0 +1,12 @@
+package misc
+
+fun main() {
+
+    // TODO convert mac address form from like 3c970e09485e into form like 3c:97:0e:09:48:5e
+    // tips: use extension function "windowed()"
+
+    fun macConvert(mac: String): String = TODO()
+
+    println(macConvert("3c970e09485e"))  // should print 3c:97:0e:09:48:5e
+
+}
