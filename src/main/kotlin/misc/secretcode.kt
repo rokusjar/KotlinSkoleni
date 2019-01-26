4bc47dc2b05f92807dae0c7af1da9c12fb333092
Marcel Matula <marcel.matula@gmail.com>
2019-01-26 20:35:52 +0100

    secret code solution

--- src/main/kotlin/misc/secretcode.kt
+++ src/main/kotlin/misc/secretcode.kt
@@ -2,13 +2,17 @@ package misc
 
 
 enum class Color {
-    RED, GREEN, WHITE, BLUE, YELLOW
+    RED, GREEN, WHITE, BLUE, YELLOW;
+
+    companion object
 }
 
 
 fun main() {
 
-    // println(Color.secretCode())
+    fun Color.Companion.secretCode() = (1..6).map { Color.values().random() }.toList()
+
+    println(Color.secretCode())
 
     // TODO implement ext. function on enum Color which generates random 6-places color code
 
