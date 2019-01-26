543640411c66c71c79e50d7c633218b89303cb83
Marcel Matula <marcel.matula@gmail.com>
2019-01-26 20:12:48 +0100

    misc safe cast todo

---
+++ src/main/kotlin/misc/safecast.kt
@@ -0,0 +1,16 @@
+package misc
+
+fun main() {
+
+    val s = TODO()  // solution s = "abc"
+    println(s as? Int)    // null
+    println(s as Int?)    // exception
+
+    // Safe casts
+    //
+    // Type cast as throws ClassCastException, if the cast is unsuccessful.
+    // Safe cast as? returns null, if the cast is unsuccessful.
+    //
+    // TODO Declare the s variable to make the first line print null and the second one throw an exception.
+
+}
