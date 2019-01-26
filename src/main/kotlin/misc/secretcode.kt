ac5f24ba566acd81962ea741af50cc65b8ee40e4
Marcel Matula <marcel.matula@gmail.com>
2019-01-26 20:31:34 +0100

    secret code todo

---
+++ src/main/kotlin/misc/secretcode.kt
@@ -0,0 +1,15 @@
+package misc
+
+
+enum class Color {
+    RED, GREEN, WHITE, BLUE, YELLOW
+}
+
+
+fun main() {
+
+    // println(Color.secretCode())
+
+    // TODO implement ext. function on enum Color which generates random 6-places color code
+
+}
