e75ecb563916e1b9684926f8bc606cf32280cb82
Marcel Matula <marcel.matula@gmail.com>
2019-01-26 20:08:07 +0100

    misc Checking identifier solution

--- src/main/kotlin/misc/checkingidentifier.kt
+++ src/main/kotlin/misc/checkingidentifier.kt
@@ -1,7 +1,5 @@
 package misc
 
-fun isValidIdentifier(id: String): Boolean = TODO()
-
 // Checking identifier
 fun main() {
 
@@ -15,3 +13,24 @@ fun main() {
 
 // TODO Implement the function that checks whether a string is a valid identifier.
 // A valid identifier is a non-empty string that starts with a letter or underscore and consists of only letters, digits and underscores.
+
+fun isValidIdentifier(id: String): Boolean {
+    fun isValidCharacter(ch: Char) = ch == '_' || ch in '0'..'9' || ch in 'a'..'z' || ch in 'A'..'Z'
+
+    if (id.isEmpty() || id[0] in '0'..'9') return false
+    for (ch in id) {
+        if (!isValidCharacter(ch)) return false
+    }
+    return true
+}
+
+fun isValidIdentifier2(id: String): Boolean {
+    fun isValidCharacter(ch: Char) = ch == '_' || ch.isLetterOrDigit()
+
+    if (id.isEmpty() || id[0].isDigit()) return false
+    for (ch in id) {
+        if (!isValidCharacter(ch)) return false
+    }
+    return true
+}
+
