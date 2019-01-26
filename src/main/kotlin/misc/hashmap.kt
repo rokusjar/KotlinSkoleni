3ad770fc32eed5458f925f312abf46cbb0a48ef8
Marcel Matula <marcel.matula@gmail.com>
2019-01-26 19:44:32 +0100

    hashmap todo task

---
+++ src/main/kotlin/misc/hashmap.kt
@@ -0,0 +1,17 @@
+package misc
+
+fun main() {
+
+    /*
+        final Map<String, String> map = new HashMap<>();
+        map.put("1", "one");
+        map.put("2", "two");
+        map.put("5", "five");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    */
+
+    // TODO prepsat do kotlinu a pouzit "Destructuring Declarations"
+
+}
