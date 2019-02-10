274839e89da62cc9df6782126ea1800d8016148c
klugi <klugi@kluganost.eu>
2019-02-10 21:40:27 +0100

    Algebraic expression - final solution.

--- src/main/kotlin/misc/algebraicExpression.kt
+++ src/main/kotlin/misc/algebraicExpression.kt
@@ -5,7 +5,7 @@ package misc
 // Nápověda: strom se konstruuje tak, že v uzlech jsou operátory a v listech jsou konkrétní číselné hodnoty,
 // konstrukce stromu pak respektuje prioritu operátorů.
 sealed class Node()
-class Value(val value: Double): Node()
+class Value(val value: Number): Node()
 class Operator(val left: Node, val right: Node, val operator: (Double, Double) -> Double) : Node()
 
 operator fun Node.plus(other: Node): Node = Operator(this, other, Double::plus)
@@ -13,28 +13,28 @@ operator fun Node.minus(other: Node): Node = Operator(this, other, Double::minus
 operator fun Node.times(other: Node): Node = Operator(this, other, Double::times)
 operator fun Node.div(other: Node): Node = Operator(this, other, Double::div)
 
-fun Double.toNode() = Value(this)
+fun Number.toNode() = Value(this)
 
 // TODO (2) implementujte funkci calculate, která tokáže takový binární strom vyhodnotit
 fun Node.calculate(): Double {
     return when(this) {
-        is Value -> this.value
+        is Value -> this.value.toDouble()
         is Operator -> this.operator(this.left.calculate(), this.right.calculate())
     }
 }
 
 fun main() {
     // TODO (3) otestujte funkci calculate na různých stromech
-    val singleValue = 3.0.toNode()
+    val singleValue = 3.toNode()
     println("Single value: ${singleValue.calculate()}")
 
-    val timesOper = singleValue * 8.0.toNode()
+    val timesOper = singleValue * 8.toNode()
     println("Times operation: ${timesOper.calculate()}")
 
     val leftSubTree = 1.2.toNode() + timesOper
     println("Left subtree: ${leftSubTree.calculate()}")
 
-    val rightSubTree = (16.0.toNode() - 0.5.toNode()) / 3.0.toNode()
+    val rightSubTree = (16.toNode() - 0.5.toNode()) / 3.toNode()
     println("Right subtree: ${rightSubTree.calculate()}")
 
     println("Final result: ${(leftSubTree + rightSubTree).calculate()}")
