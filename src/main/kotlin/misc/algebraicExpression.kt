99f40a46ad700921162544526bd9543d4d5e6a77
klugi <klugi@kluganost.eu>
2019-02-10 14:25:23 +0100

    Algebraic expression task.

---
+++ src/main/kotlin/misc/algebraicExpression.kt
@@ -0,0 +1,12 @@
+package misc
+
+// TODO (1) reprezentujte v Kotlinu binární strom, který bude reprezentovat obecný algebraický výraz.
+// Příklad takového výrazu: 1.2 + 3 * 8 + (16 - 0.5) / 3
+// Nápověda: strom se konstruuje tak, že v uzlech jsou operátory a v listech jsou konkrétní číselné hodnoty,
+// konstrukce stromu pak respektuje prioritu operátorů.
+
+// TODO (2) implementujte funkci calculate, která tokáže takový binární strom vyhodnotit
+
+fun main() {
+    // TODO (3) otestujte funkci calculate na různých stromech
+}
