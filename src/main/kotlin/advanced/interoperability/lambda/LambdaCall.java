68c41a62581389cd5700440f4022001e45eebbab
Jaromír Rokusek <jrokusek@profinit.eu>
2019-02-12 15:02:07 +0100

    kotlin advanced - kotlin java interoperability lambda solution

--- src/main/kotlin/advanced/interoperability/lambda/LambdaCall.java
+++ src/main/kotlin/advanced/interoperability/lambda/LambdaCall.java
@@ -1,7 +1,19 @@
 package advanced.interoperability.lambda;
 
+import kotlin.jvm.functions.Function1;
+
 public class LambdaCall {
     public static void main(String[] args) {
         // TODO (1) Zavolejte funkci foo ze souboru lambda
+        LambdaKt.foo("Hello World", String::toUpperCase);
+
+        LambdaKt.foo("Hello World Again", text -> text + " :)");
+
+        LambdaKt.foo("Hello World Yet Again", new Function1<String, String>() {
+            @Override
+            public String invoke(String s) {
+                return s.toLowerCase();
+            }
+        });
     }
 }
