19adaf285350cebfecea33fe4185cc9c9065a6d2
Marcel Matula <marcel.matula@gmail.com>
2019-02-20 13:56:03 +0100

    Concurrency sleep sort

---
+++ src/main/kotlin/advanced/concurrency/sleepsort.kt
@@ -0,0 +1,11 @@
+package advanced.concurrency
+
+fun main() {
+
+    // TODO Write a program that implements sleep sort.
+    // for each number N from list is created thread which executes command to sleep for N second and after N is printed
+    fun sleepSort(numbers: List<Int>) {
+        TODO()
+    }
+
+}
