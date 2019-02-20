35be35714f6f0a86c0f24e2086929ad184d2ac42
Marcel Matula <marcel.matula@gmail.com>
2019-02-20 10:41:44 +0100

    concurrency - race condition

---
+++ src/main/kotlin/advanced/concurrency/racecondition.kt
@@ -0,0 +1,21 @@
+package advanced.concurrency
+
+
+fun main() {
+
+    var count = 0
+
+    // TODO (1) - creates list of 2000 threads - running block will be "count++"
+    // be sure that threads are created but not started
+
+    // TODO (2) - for each thread call start()
+
+    // TODO (3) - wait for all threads finished their job - use join() method
+
+    println("count: $count")
+
+    // TODO (4) - solve race condition - print should output each time 2000 (not less)
+
+
+}
+
