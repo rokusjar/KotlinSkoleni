4bd49da50abbe686d0ceabb3faf26fed366df18c
Marcel Matula <marcel.matula@gmail.com>
2019-02-21 08:14:41 +0100

    Concurrency withTimeoutLock  - fix for last().join() using forEach join or latch

--- src/main/kotlin/advanced/concurrency/racecondition.kt
+++ src/main/kotlin/advanced/concurrency/racecondition.kt
@@ -1,5 +1,6 @@
 package advanced.concurrency
 
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.thread
@@ -8,61 +9,73 @@ import kotlin.concurrent.withLock
 
 fun main() {
 
-    var count = 0
+    val threadsCount = 2000
+    run {
+        var count = 0
 
-    // TODO (1) - creates list of 2000 threads - running block will be "count++"
-    // be sure that threads are created but not started
-    val threads = List(2000) {
-        thread(start = false) { count++ }
+        // TODO (1) - creates list of 2000 threads - running block will be "count++"
+        // be sure that threads are created but not started
+        val threads = List(threadsCount) {
+            thread(start = false) { count++ }
+        }
+        // TODO (2) - for each thread call start()
+        threads.forEach { it.start() }
+
+        // TODO (3) - wait for all threads finished their job
+        threads.forEach { it.join() }
+
+        println("count: $count")
     }
-
-    // TODO (2) - for each thread call start()
-    threads.forEach { it.start() }
-
-    // TODO (3) - wait for all threads finished their job
-    threads.last().join()
-
-    println("count: $count")
 
     // TODO (4) - solve race condition - print should output each time 2000 (not less)
 
     /** SOLUTION I - using lock */
-    val lock = ReentrantLock()
-    count = 0
-    List(2000) {
-        thread(start = false) {
-            lock.withLock { count++ }
+    run {
+        val lock = ReentrantLock()
+        var count = 0
+        List(threadsCount) {
+            thread(start = false) {
+                lock.withLock { count++ }
+            }
+        }.run {
+            forEach { it.start() }
+            forEach { it.join() }
         }
-    }.run {
-        forEach { it.start() }
-        last().join()
+        println("I: count: $count")
     }
-    println("I: count: $count")
 
-    /** SOLUTION II - using synchronized */
-    val lockObject = Any()
-    count = 0
-    List(2000) {
-        thread(start = false) {
-            synchronized(lockObject) { count++ }
+    /** SOLUTION II - using synchronized + latch*/
+    run {
+        val lockObject = Any()
+        val latch = CountDownLatch(threadsCount)
+        var count = 0
+        List(threadsCount) {
+            thread(start = false) {
+                synchronized(lockObject) { count++ }
+                latch.countDown()
+            }
+        }.run {
+            forEach { it.start() }
+            latch.await()
         }
-    }.run {
-        forEach { it.start() }
-        last().join()
+        println("II. count: $count")
     }
-    println("II. count: $count")
 
-    /** SOLUTION III - using atomic integer */
-    val count2 = AtomicInteger(0)
-    List(2000) {
-        thread(start = false) {
-            count2.incrementAndGet()
+    /** SOLUTION III - using atomic integer + latch*/
+    run {
+        val count = AtomicInteger(0)
+        val latch = CountDownLatch(threadsCount)
+        List(threadsCount) {
+            thread(start = false) {
+                count.incrementAndGet()
+                latch.countDown()
+            }
+        }.run {
+            forEach { it.start() }
+            latch.await()
         }
-    }.run {
-        forEach { it.start() }
-        last().join()
+        println("III. count: $count")
     }
-    println("III. count: $count2")
 
 
 }
