24e49917d746d9c4340931c0e21c86a48ca030be
Marcel Matula <marcel.matula@gmail.com>
2019-02-28 10:58:13 +0100

    advanced collection added factorial sequence

--- src/main/kotlin/advanced/collections/sequence.kt
+++ src/main/kotlin/advanced/collections/sequence.kt
@@ -1,7 +1,6 @@
 package advanced.collections
 
 import java.util.concurrent.TimeUnit
-import kotlin.system.measureTimeMillis
 
 fun heavyCalculation(number: Int): Int {
     TimeUnit.SECONDS.sleep(1)
@@ -14,6 +13,9 @@ fun main() {
     // TODO (1.2) změř dobu běhu programu (nápověda: můžeš použít globální funkci measureTimeMillis)
     // TODO (1.3) proveď stejné operace nad sekvencí čísel od 1 do 10 a porovnej dobu běhu
 
-    // TODO (2) vytvoř generátor fibonnacciho posloupnosti (1,1,2,3,5 - hodnota následujícího prvku je součtem dvou předchozích),
+    // TODO (2) vytvoř generátor na faktorial posloupnost (1,2,6,24 ... 1-ni prvek je 1 a pak n-ty prvek = n * factorial(n-1),
+    //  vytiskni prvních 8 prvků této sekvence
+
+    // TODO (3) vytvoř generátor fibonnacciho posloupnosti (1,1,2,3,5 - hodnota následujícího prvku je součtem dvou předchozích),
     //  vytiskni prvních 12 prvků této sekvence
 }
