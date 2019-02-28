4ebeed42a34ad2c4d23ed017fc7ecaee00444f57
Marcel Matula <marcel.matula@gmail.com>
2019-02-28 10:57:26 +0100

    advadnced collection added factorial sequence

--- src/main/kotlin/advanced/collections/sequence.kt
+++ src/main/kotlin/advanced/collections/sequence.kt
@@ -23,8 +23,14 @@ fun main() {
     }
     println("Calculation took $time ms")
 
-    // TODO (2) vytvoř generátor fibonnacciho posloupnosti (1,1,2,3,5 - hodnota následujícího prvku je součtem dvou předchozích),
+    // TODO (2) vytvoř generátor na faktorial posloupnost (1,2,6,24 ... 1-ni prvek je 1 a pak n-ty prvek = n * factorial(n-1),
+    //  vytiskni prvních 8 prvků této sekvence
+    var count = 1
+    val factorial = generateSequence(1 ) { it * ++count }
+    factorial.take(8).forEach { println(it) }
+
+    // TODO (3) vytvoř generátor fibonnacciho posloupnosti (1,1,2,3,5 - hodnota následujícího prvku je součtem dvou předchozích),
     //  vytiskni prvních 12 prvků této sekvence
     val fibonacci = generateSequence(1 to 1) { it.second to it.first + it.second }.map { it.first }
-    fibonacci.take(12).toList().apply { println(this) }
+    fibonacci.take(12).forEach { println(it) }
 }
